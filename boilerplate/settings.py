4ee85f68ccdcc64752aa8c29de973866c8ed9b57
Godswill <30760648+WillDera@users.noreply.github.com>
2020-07-07 22:49:40 +0100

    Update
    - Generated static files

--- boilerplate/settings.py
+++ boilerplate/settings.py
@@ -120,6 +120,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIR = [os.path.join(BASE_DIR, 'static_files')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # DEBUG TOOLBAR SETTINGS
 
